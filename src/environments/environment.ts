9637343c134be5465319ba772a2d4d6fa9cbc3f8
root <root@srv1050792.hstgr.cloud>
2025-10-21 14:14:40 +0000

    Change From VPS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api/',
-  // apiUrl : "https://muahhideen.onrender.com/api/"
-  // jitsiDomain: 'localhost:8080',
-  jitsiDomain: 'meet.localhost:8443',
-  // jitsiDomain: '192.168.1.70:8443',
-  // jitsiDomain: '192.168.1.70:8443',  // LAN IP
+  apiUrl: 'https://alkutab.dirnia.click/api/',
+  jitsiDomain: 'alkutab.dirnia.click:8447',
 };
